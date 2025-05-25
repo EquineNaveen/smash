aac118c035d1c83a3cb3a82fce60ae3f2d1479a5
EquineNaveen <snaveengowda13063@gmail.com>
2025-05-25 12:19:57 +0530

    description added

--- pages/apps.py
+++ pages/apps.py
@@ -226,10 +226,10 @@ if username:
 
 # --- App Cards ---
 cards = [
-    {"name": "Coder<br>fg", "img": "artifacts/1.jpg", "link": "http://192.168.31.13:8502"},
-    {"name": "Document<br>", "img": "artifacts/doc.jpg", "link": "#"},
-    {"name": "Meeting<br>", "img": "artifacts/meeting.jpg", "link": "#"},
-    {"name": "Admin<br>", "img": "artifacts/admin.png", "link": "#"},
+    {"name": "GYAAN CODER<br><span style='font-size:0.85em'>Gyaan Coder provides real-time answers and code solutions for user queries across any programming language.</span>", "img": "artifacts/1.jpg", "link": "http://192.168.31.13:8502"},
+    {"name": "GYAAN DOC<br><span style='font-size:0.85em'>Gyaan Doc is an AI-powered app that lets you upload documents, get instant summaries, and ask questions based on their content.</span>", "img": "artifacts/doc.jpg", "link": "#"},
+    {"name": "GYAAN MEETING<br><span style='font-size:0.85em'>Gyaan Meeting converts meeting audio into text, provides transcripts, and lets you query information directly from the conversation.</span>", "img": "artifacts/meeting.jpg", "link": "#"},
+    {"name": "GYAAN ADMIN<br><span style='font-size:0.85em'>Gyaan Admin is a chatbot to streamline admin tasks and answer queries on operations, policies, and processes.</span>", "img": "artifacts/admin.png", "link": "#"},
 ]
 
 def get_base64_image(image_path):
